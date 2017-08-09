fbb33c56d4e25c4c6f9e732e388026d76bd5ee97
mlamont8 <debiandmiat@hotmail.com>
2017-08-09 16:56:08 -0400

    updated header

---
+++ src/components/header/header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Navbar, Nav ,NavItem } from 'react-bootstrap';
+
+
+class Header extends React.Component{
+
+  render() {
+    return (
+      <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              Flickr Search
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">Link Right</NavItem>
+              <NavItem eventKey={2} href="#">Link Right</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+    );
+  }
+
+};
+
+export default Header;
