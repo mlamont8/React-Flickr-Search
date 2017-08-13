a7ce0c11b2a1389728133e200b5ceec98a9f0eb4
mlamont8 <debiandmiat@hotmail.com>
2017-08-13 15:06:57 -0400

    api pulls results into state

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -1,34 +1,72 @@
 import React from 'react';
 import queryString from 'query-string';
+import axios from 'axios';
 
 class Grid extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      results: [],
+      term: ''
+    };
+
     this.urlParse = this.urlParse.bind(this);
+
   }
+
+
+  // For initial render
+    componentDidMount() {
+        this.urlParse();
+    }
+
+  // If user enters new search term within this component
+    componentDidUpdate(nextProps){
+      //Check to make sure search term actually changed
+      if(JSON.stringify(this.props.location.search) !== JSON.stringify(nextProps.location.search))
+        {
+          this.urlParse();
+    }
+  }
+
+
+  apiPull(search) {
+    console.log(search);
+     return axios.get('https://api.flickr.com/services/rest',{
+      params: {
+      method: 'flickr.photos.search',
+      api_key: '1dfbed40745d1f76dc935c78c30abf16',
+      text: search,
+      safe_search: '1',
+      per_page: '50',
+      format: 'json',
+      nojsoncallback: 1
+  }
+    })
+    .then(function (response) {
+      console.log(response.data);
+      this.setState({
+        results: response.data.photos.photo
+      })
+    }.bind(this)
+  )
+    .catch(function (error) {
+      console.log('Main Search error',error);
+    });
+  }
+
 
 //Get search term from URL
   urlParse() {
       let gridTerm = queryString.parse(this.props.location.search)
-      console.log(gridTerm.searchTerm);
+      // pull from flickr api
+      this.apiPull(gridTerm.searchTerm);
+      this.setState({
+        term: gridTerm.searchTerm
+      })
 
   }
-
-// For initial render
-  componentDidMount() {
-      this.urlParse();
-  }
-
-// If user enters new search term within this component
-  componentDidUpdate(nextProps){
-    //Check to make sure search term actually changed
-    if(JSON.stringify(this.props.location.search) !== JSON.stringify(nextProps.location.search))
-      {
-        this.urlParse();
-  }
-}
-
 
 
   render() {
