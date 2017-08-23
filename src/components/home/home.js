2aba2d404b23a85728019771dad2210f7b2fe725
mlamont8 <debiandmiat@hotmail.com>
2017-08-23 09:16:51 -0400

    added images

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import img1 from '../../images/img1.jpg'
 
 
 class Home extends React.Component{
 
+  constructor(props) {
+    super(props);
+    this.state = {backgroundImage: img1};
+  }
 
   render() {
+    let backgroundUrl = {
+  "background-image" : "url(../images/" + this.state.backgroundImage +")"
+}
     return (
-      <div className="home-container">
+      <div className="home-container" bsstyle={backgroundUrl}>
         <h1>Home Component</h1>
       </div>
     )
