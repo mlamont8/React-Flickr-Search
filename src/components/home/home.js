4e34f9a1fca3671698f6b4a6c5cec54ab15b2b2c
mlamont8 <debiandmiat@hotmail.com>
2017-08-23 22:01:12 -0400

    updated background code

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import img1 from '../../images/img1.jpg'
 
 
 class Home extends React.Component{
@@ -9,26 +8,11 @@ class Home extends React.Component{
     this.state = {backgroundImage: ''};
   }
 
-  componentDidMount() {
-
-      // generate a random number from  1 and 4
-      let randImage=Math.floor(Math.random()*5)
-      //put image link portion in state
-      this.setState({
-        // backgroundImage: 'img' + randImage,
-        backgroundImage: img1
-
-      })
-    }
-
-
 
   render() {
-    let backgroundUrl = {
-  "backgroundImage" : "url(" + this.state.backgroundImage +")"
-}
+
     return (
-      <div className="home-container" style={backgroundUrl}>
+      <div className="home-container">
       </div>
     )
   }
