b6ec1e6e3ace23f4f764980e85992fa3c12403b9
M L Windows <debiandmiat@hotmail.com>
2017-08-30 18:28:11 -0400

    changed grid color and image size

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -8,7 +8,7 @@ class Cards extends React.Component {
   render() {
     const cardUrl = 'https://farm' + this.props.cardItem.farm + '.staticflickr.com/' +
       this.props.cardItem.server + '/' + this.props.cardItem.id + '_' + this.props.cardItem.secret +
-      '_n.jpg';
+      '_z.jpg';
     return(
       <div className='image-container'>
            <Image className="image" src = {cardUrl} responsive />
