24a1d3d9cc320f1e5a72a23e561521e96f8826f0
mlamont8 <mlamont8@hotmail.com>
2018-08-06 10:56:25 -0400

    minor code updates

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,75 +1,74 @@
-import React from 'react';
-import { Navbar, FormGroup, FormControl, Button } from 'react-bootstrap';
-import { Redirect, Link } from 'react-router-dom';
+import React from "react";
+import { Navbar, FormGroup, FormControl, Button } from "react-bootstrap";
+import { Redirect, Link } from "react-router-dom";
 
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      searchTerm: "",
+      formSubmit: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-class Header extends React.Component{
-constructor(props) {
-  super(props);
-  this.state = {
-    value: '',
-    searchTerm: '',
-    formSubmit: false
-  };
-  this.handleChange = this.handleChange.bind(this);
-  this.handleSubmit = this.handleSubmit.bind(this);
-}
+  handleChange(event) {
+    this.setState({
+      searchTerm: "",
+      value: event.target.value,
+      formSubmit: false
+    });
+  }
 
-handleChange(event) {
-  this.setState({
-    searchTerm: '',
-    value: event.target.value,
-    formSubmit: false
-  })
-}
-
-handleSubmit(event) {
-  event.preventDefault();
-  this.setState({
-    searchTerm: this.state.value,
-    value: '',
-    formSubmit: true
-  })
-}
-
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      searchTerm: this.state.value,
+      value: "",
+      formSubmit: true
+    });
+  }
 
   render() {
     // The new url after search term is submitted
-    const searchURL = '?searchTerm='+ this.state.searchTerm;
+    const searchURL = "?searchTerm=" + this.state.searchTerm;
     return (
-      <Navbar collapseOnSelect className='mainNav'>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">Flickr Image Search</Link>
-            </Navbar.Brand>
+      <Navbar collapseOnSelect className="mainNav">
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Flickr Image Search</Link>
+          </Navbar.Brand>
           <Navbar.Toggle />
-          </Navbar.Header>
+        </Navbar.Header>
 
-          <Navbar.Collapse>
-            <Navbar.Form pullRight>
-              <form onSubmit={this.handleSubmit}>
-                <FormGroup>
-                  <FormControl type="text" placeholder="Search" value={this.state.value} onChange={this.handleChange}/>
-                    {' '}
-                  <Button type="submit">Submit</Button>
-                </FormGroup>
-              </form>
-              </Navbar.Form>
+        <Navbar.Collapse>
+          <Navbar.Form pullRight>
+            <form onSubmit={this.handleSubmit}>
+              <FormGroup>
+                <FormControl
+                  type="text"
+                  placeholder="Search"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+            </form>
+          </Navbar.Form>
 
-              {this.state.formSubmit && (
-                <Redirect to={{
-                  pathname: '/results',
-                  search: searchURL
-                }}/>
-              )}
-          </Navbar.Collapse>
-
-        </Navbar>
-
-
+          {this.state.formSubmit && (
+            <Redirect
+              to={{
+                pathname: "/results",
+                search: searchURL
+              }}
+            />
+          )}
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
-
-};
+}
 
 export default Header;
