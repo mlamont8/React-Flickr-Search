957ed984481251faa7b9bd17237b665d197ccae7
Lamont <mlamont8@hotmail.com>
2018-09-18 21:48:54 -0400

    changed frontpage search form

---
+++ src/components/animatedButton/AnimatedButton.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AnimatedButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { btnToggle: false };
+        this.handleClick = this.handleClick.bind(this);
+      }
+
+      handleClick() {
+        this.setState({
+          btnToggle: !this.state.btnToggle
+        })
+      }
+
+        render() {
+            const formActive = this.state.btnToggle ? 'animatedInput active' : 'animatedInput';
+            const btnClose = this.state.btnToggle ? 'inputBtn close' : 'inputBtn';
+            return (
+              <div className="homeContainer">
+                <div className="homeMiddle">
+            
+                <form className="search-box" onSubmit={this.props.handleSubmit}>
+                  <input type="text" 
+                    value={this.props.value}
+                    onChange={this.props.handleChange}
+                    className={formActive}/>
+                  <button 
+                    type="button" 
+                    className={btnClose}
+                    onClick={this.handleClick}></button>
+                </form>
+                </div>
+            </div>
+            );
+          }
+        }
+
+        AnimatedButton.propTypes = {
+          handleSubmit: PropTypes.func,
+          handleChange: PropTypes.func,
+          value: PropTypes.string,
+        }
+
+
+export default AnimatedButton;
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,21 +1,29 @@
 import React from "react";
-import InputForm from "../inputForm/inputForm";
+import AnimatedButton from "../animatedButton/AnimatedButton";
+import PropTypes from 'prop-types';
 
 class Home extends React.Component {
+
   render() {
     return (
-      <div className="home-container container-fluid">
-        <h1>QUICKR IMAGE SEARCH</h1>
-        <div className="homeForm">
-          <InputForm
-            handleSubmit={this.props.handleSubmit}
-            value={this.props.value}
-            handleChange={this.props.handleChange}
-          />
-        </div>
+  
+      <div className="home-container">
+        <AnimatedButton
+          handleSubmit={this.props.handleSubmit}
+          value={this.props.value}
+          handleChange={this.props.handleChange}
+       />
       </div>
     );
   }
+}
+
+Home.propTypes = {
+  handleChange: PropTypes.func,
+  handleSubmit: PropTypes.func,
+  value: PropTypes.string,
+  searchTerm: PropTypes.string,
+  formSubmit: PropTypes.string,
 }
 
 export default Home;
